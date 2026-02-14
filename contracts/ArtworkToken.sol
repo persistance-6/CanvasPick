a0e39ee6dbfad3fdfbebc59bd5e4ee63fa5ac451
persistance-6 <yunjinu1224@gmail.com>
2026-02-14 10:57:18 +0000

    feat: ArtworkToken contract 생성 및 registerArtwork 함수 구현
    - ERC721 기반 스마트 컨트랙트 설계
    - Artwork struct 정의 (tokenId, name, artist, location, creationDate, pricePerPiece)
    - registerArtwork 함수: onlyOwner 제약으로 새 작품 등록
    - getArtwork 함수: 작품 정보 조회
    - nextTokenId를 통한 자동 증가 ID 관리

---
+++ contracts/ArtworkToken.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.0;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ArtworkToken is ERC721, Ownable {
+    struct Artwork {
+        uint256 tokenId;
+        string name;
+        string artist;
+        string location;
+        uint256 creationDate;
+        uint256 pricePerPiece;
+    }
+
+    uint256 public nextTokenId = 1;
+
+    mapping (uint256 => Artwork) public artworks;
+
+    constructor() ERC721("ArtworkToken", "ARTWORK") Ownable(msg.sender) {}
+
+    function registerArtwork(
+        string memory name,
+        string memory artist,
+        string memory location,
+        uint256 creationDate,
+        uint256 pricePerPiece
+    ) public onlyOwner returns (uint256) {
+        require(bytes(name).length > 0, "Name is required");
+        require(bytes(artist).length > 0, "Artist is required");
+        require(bytes(location).length > 0, "Location is required");
+        require(creationDate > 0, "Creation date is required");
+        require(pricePerPiece > 0, "Price per piece must be greater than zero");
+
+        // Mint the artwork NFT to the owner
+        _mint(msg.sender, nextTokenId);
+        
+        artworks[nextTokenId] = Artwork({
+            tokenId: nextTokenId,
+            name: name,
+            artist: artist,
+            location: location,
+            creationDate: creationDate,
+            pricePerPiece: pricePerPiece
+        });
+
+        nextTokenId++;
+        return nextTokenId - 1;
+    }
+
+    function getArtwork(uint256 tokenId) public view returns (Artwork memory) {
+        require(artworks[tokenId].tokenId > 0, "Artwork does not exist");
+        return artworks[tokenId];
+    }
+}
